d1090f03ace837aeacf1e5582b796419d6b00710
Kieran Briggs <kieran.briggs@sheffcol.ac.uk>
2012-12-20 14:54:52 +0000

    Change to layout styles
    changes to the size of the icons, and spacing around course titles
    
    Signed-off-by: Kieran Briggs <kieran.briggs@sheffcol.ac.uk>

--- renderer.php
+++ renderer.php
@@ -107,7 +107,7 @@ class block_course_overview_plus_renderer extends plugin_renderer_base {
             $attributes = array('title' => s($course->fullname));
             if ($course->id > 0) {
                 $link = html_writer::link(new moodle_url('/course/view.php', array('id' => $course->id)), format_string($course->shortname, true, $course->id), $attributes);
-                $html .= $this->output->heading($link, 2, 'title');
+                $html .= $this->output->heading($link, 3, 'title');
             } else {
                 $html .= $this->output->heading(html_writer::link(
                     new moodle_url('/auth/mnet/jump.php', array('hostid' => $course->hostid, 'wantsurl' => '/course/view.php?id='.$course->remoteid)),
@@ -149,7 +149,7 @@ class block_course_overview_plus_renderer extends plugin_renderer_base {
             $output .= html_writer::start_tag('div', array('class' => 'activity_overview'));
             $url = new moodle_url("/mod/$module/index.php", array('id' => $cid));
             $modulename = get_string('modulename', $module);
-            $icontext = html_writer::link($url, $this->output->pix_icon('icon', $modulename, 'mod_'.$module, array('class'=>'iconlarge')));
+            $icontext = html_writer::link($url, $this->output->pix_icon('icon', $modulename, 'mod_'.$module, array('class'=>'icon')));
             if (get_string_manager()->string_exists("activityoverview", $module)) {
                 $icontext .= get_string("activityoverview", $module);
             } else {
